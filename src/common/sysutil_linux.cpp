5938e9520b7ea8c9f0891c974b223543ed95e2f4
Yaroslav Chvanov <yaroslav.chvanov@gmail.com>
2023-03-26 18:54:45 +0300

    Use strncpy instead of strcpy with incorrect signature

--- src/common/sysutil_linux.cpp
+++ src/common/sysutil_linux.cpp
@@ -26,7 +26,7 @@ void GetExecutablePath(char* output, size_t bufsize, size_t& len)
 
     using namespace std::filesystem;
     auto parent = path(fullpath).parent_path();
-    strcpy(output, bufsize, (const char*)parent.u8string().c_str());
+    strncpy(output, (const char*)parent.u8string().c_str(), bufsize);
     len = strlen(output);
 }
 
